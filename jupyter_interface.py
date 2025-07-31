7dab9be847a3eaeacebdc52e75185621e766bedd
cas003 <cas003@usask.ca>
2025-07-31 12:03:39 -0600

    Fixed download implementation
    Much better now, a lot more readable, and expandable.
    Will try hashing to improve speed performance of download

--- jupyter_interface.py
+++ jupyter_interface.py
@@ -122,7 +122,7 @@ class JupyterInterface:
         else:
             print("No connection to server")
 
-    def download_info(self, overwrite = False):
+    def download_info_old(self, overwrite = False):
         # TODO: Only download data if we know that it has converged. (Can be based on the xspec folder perhaps)
         # TODO: Could this be implemented by using a bash script that does it all at once. Create a local hash that sees whether it needs to download new folders. No need to check local versions.
         # TODO: Issue if people want to manually download their data and then convert to h5...
@@ -141,7 +141,10 @@ class JupyterInterface:
                     #c.run(f'find -type d | grep {folder_name} > foldernames.txt')
                     c.run(f'find ~+ -type f | grep {folder_name}_ > foldernames.txt')
                     c.get(f"{self.working_directory}/foldernames.txt")
-                    c.get(f"{self.working_directory}/checksums.txt")
+                    try:
+                        c.get(f"{self.working_directory}/checksums.txt")
+                    except:
+                        pass
                     current_folder = os.getcwd()
                     storage_folder = f"StorageFor{folder_name}"
                     Path(storage_folder).mkdir(exist_ok=True)
@@ -162,7 +165,7 @@ class JupyterInterface:
 
                             # The location of the file on the local computer and where we want to put it.
                             file_location = current_folder + '/' + storage_folder + '/' + Path(line.strip()).parent.name + '/' + Path(line.strip()).name
-                            if overwrite:
+                            """ if overwrite:
                                 Path(storage_folder + '/' + Path(line.strip()).parent.name).mkdir(exist_ok=True)
                                 c.get(line.strip(), file_location)
                             else:
@@ -172,10 +175,12 @@ class JupyterInterface:
                                 else:
                                     #print("File already exists")
                                     pass
-
+"""
                             # TODO: Create something that uses grep to find the parameters and stores in a file before we h5 it???
                             # Check if group exists, if it does then we can delete it and overwrite it with new data???
                             if Path(line.strip()).suffix == '.in1':  # This is for RKMax, Emin/Emax
+                                Path(storage_folder + '/' + Path(line.strip()).parent.name).mkdir(exist_ok=True)
+                                c.get(line.strip(), file_location)
                                 with open(file_location, 'r') as in1:
                                     in1_lines = in1.readlines()
                                     rkmax = in1_lines[1].split()[0] # Get the second line, first value, which is rkmax
@@ -184,12 +189,20 @@ class JupyterInterface:
                                 file.create_dataset(f"{Path(line.strip()).stem}/parameters/rkmax",data=rkmax)
                                 file.create_dataset(f"{Path(line.strip()).stem}/parameters/inputEnergyRange", data=[emin, emax])
                             if Path(line.strip()).suffix == '.in2':  # GMAX
+                                Path(storage_folder + '/' + Path(line.strip()).parent.name).mkdir(exist_ok=True)
+                                c.get(line.strip(), file_location)
                                 print(line)
                             if Path(line.strip()).suffix == '.klist':  # Get the number of k points, as well as k grid
+                                Path(storage_folder + '/' + Path(line.strip()).parent.name).mkdir(exist_ok=True)
+                                c.get(line.strip(), file_location)
                                 print(line)
                             if Path(line.strip()).suffix == '.outputd':  # Has everything. gmin, gmax, lattice constants, atoms in unit cell, rkmax
+                                Path(storage_folder + '/' + Path(line.strip()).parent.name).mkdir(exist_ok=True)
+                                c.get(line.strip(), file_location)
                                 print(line)
                             if Path(line.strip()).suffix == '.scf2':  # This is for :GAP, :FER, high/low energy sep
+                                Path(storage_folder + '/' + Path(line.strip()).parent.name).mkdir(exist_ok=True)
+                                c.get(line.strip(), file_location)
                                 with open(file_location, 'r') as scf2:
                                     scf2_lines = scf2.readlines()
                                     for i in scf2_lines:
@@ -205,6 +218,8 @@ class JupyterInterface:
                                             file.create_dataset(f"{Path(line.strip()).stem}/parameters/energystatesSeparation", data=low_high_sep)
 
                             if Path(line.strip()).suffix == '.scfc':  # Has the energy of the core states, 1S, 2S, 2P etc.
+                                Path(storage_folder + '/' + Path(line.strip()).parent.name).mkdir(exist_ok=True)
+                                c.get(line.strip(), file_location)
                                 print(line)
 
                 with h5py.File(Path(self.cif_file).stem + ".hdf5", 'r') as file:
@@ -222,7 +237,7 @@ class JupyterInterface:
         else:
             print("No connection to server")
 
-    def convert_to_hdf5(self):
+    def convert_to_hdf5_old(self):
         with h5py.File(Path(self.cif_file).stem + ".hdf5", 'w') as file: # Main file to store all info
             # Iterate over all folders in the storage
             case = file.create_group("ToCv2_006")
@@ -238,6 +253,85 @@ class JupyterInterface:
             # If they exist, collect the dos
             dos = case.create_group("dos")
             print("Converting to HDF5")
+
+    def download_info(self, overwrite = False):
+        if self.server_connection is not None:
+            with self.server_connection as c:  # This will open and close connection automatically
+                print("Starting download")
+                start_time = time.perf_counter()
+
+                with c.cd(self.working_directory):
+                    folder_name = Path(self.cif_file).stem
+                    c.run(f'find ~+ -type f | grep {folder_name}_ > foldernames.txt')
+                    c.get(f"{self.working_directory}/foldernames.txt")
+
+                current_folder = os.getcwd()
+                storage_folder = f"StorageFor{folder_name}"
+                Path(storage_folder).mkdir(exist_ok=True)
+
+                with open("foldernames.txt", 'r') as f:
+                    for line in f:
+                        # TODO: Might break in windows with the different slash directions
+                        # TODO: Figure out how to check if it changed from previous versions. So, using md5sum likely.
+
+                        # The location of the file on the local computer and where we want to put it.
+                        # NOTE: We use strip to remove the \n character at the end of the line in the txt file
+                        file_location = current_folder + '/' + storage_folder + '/' + Path(line.strip()).parent.name + '/' + Path(line.strip()).name
+                        files_we_want = ['.in1','.in2','.klist','.outputd','.scf2','.scfc','.txspec']
+                        if Path(line.strip()).suffix in files_we_want:  # This is for RKMax, Emin/Emax
+                            Path(storage_folder + '/' + Path(line.strip()).parent.name).mkdir(exist_ok=True)
+                            c.get(line.strip(), file_location)
+                            self.convert_to_hdf5(file_location)
+                end_time = time.perf_counter()
+                elapsed_time = end_time - start_time
+                print(f"Execution time: {elapsed_time:.4f} seconds")
+                print("Download complete")
+        else:
+            print("No connection to server")
+        return
+
+    def convert_to_hdf5(self, file_name, overwrite = False):
+        # This parses through the files and gets the info we care about
+        case_name = Path(file_name.strip()).stem
+        if Path(file_name.strip()).suffix == '.in1':  # This is for RKMax, Emin/Emax
+            with open(file_name, 'r') as in1:
+                in1_lines = in1.readlines()
+                rkmax = in1_lines[1].split()[0]  # Get the second line, first value, which is rkmax
+                emin = in1_lines[-1].split()[3]  # Last line, second value
+                emax = in1_lines[-1].split()[4]  # Last line, third value
+            self.create_dataset(f"{case_name}/parameters/rkmax", data=rkmax)
+            self.create_dataset(f"{case_name}/parameters/inputEnergyRange", data=[emin, emax])
+        elif Path(file_name.strip()).suffix == '.scf2':  # This is for :GAP, :FER, high/low energy sep
+            with open(file_name, 'r') as scf2:
+                scf2_lines = scf2.readlines()
+                for i in scf2_lines:
+                    if re.search(":GAP \(global\)", i):  # TODO: Spin polarized has GAP (this spin)
+                        gap_ry = i.split()[3]
+                        gap_ev = i.split()[6]
+                        self.create_dataset(f"{case_name}/parameters/bandgap", data=[gap_ry, gap_ev])
+                    elif re.search(":FER", i):
+                        fermi = i.split()[9]
+                        self.create_dataset(f"{case_name}/parameters/fermi", data=fermi)
+                    elif re.search("Energy to separate low and high energystates", i):
+                        low_high_sep = i.split()[7]
+                        self.create_dataset(f"{case_name}/parameters/energystatesSeparation", data=low_high_sep)
+
+
+    def print_h5_structure(self):
+        with h5py.File(Path(self.cif_file).stem + ".hdf5", 'r') as file:
+            # for name in file:
+            #    print(name)
+            def printname(name):
+                print(name)
+            file.visit(printname)
+
+    def create_dataset(self, dataset_name, **kwargs):
+        with h5py.File(Path(self.cif_file).stem + ".hdf5", 'a') as h5_file: # Main file to store all info
+            if dataset_name in h5_file.keys():
+                del h5_file[dataset_name]
+                h5_file.create_dataset(dataset_name, **kwargs)
+            else:
+                h5_file.create_dataset(dataset_name, **kwargs)
 
 # H5 Data Storage
 """
